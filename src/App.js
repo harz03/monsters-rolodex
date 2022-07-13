9a02d8ec15e0f133ee242a7199c4aaba6ea7b8be
Harshit <harshit79090@gmail.com>
2022-07-13 21:16:21 +0530

    revising with state and components

--- src/App.js
+++ src/App.js
@@ -5,14 +5,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Linda',
+        },
+        {
+          name: 'Vodelolka',
+        },
+        {
+          name: 'RumRum',
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>Hi Harshit</p>
-          <button>Change Name</button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return <h1>{monster.name}</h1>;
+        })}
       </div>
     );
   }
