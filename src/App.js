aebac34db09d76dc3d0eb4f663845d565d20b9a1
Harshit <harshit79090@gmail.com>
2022-07-16 11:20:37 +0530

    Made code more readable

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import { Component } from 'react';
 //Components are independent and reusable bits of code. They serve the same purpose as JavaScript functions, but work in isolation and return HTML.
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
+    console.log('constructor');
 
     this.state = {
       monsters: [],
       searchString: '',
     };
   }
-
+  OnSearchChange = (event) => {
+    const searchString = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchString };
+    });
+  };
   componentDidMount() {
+    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) =>
@@ -30,8 +36,13 @@ class App extends Component {
   }
 
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchString);
+    console.log('render');
+
+    const { monsters, searchString } = this.state;
+    const { OnSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchString);
     });
     return (
       <div className='App'>
@@ -39,12 +50,7 @@ class App extends Component {
           className='search-box'
           type='search'
           placeholder='Search Monster'
-          onChange={(event) => {
-            const searchString = event.target.value.toLocaleLowerCase();
-            this.setState(() => {
-              return { searchString };
-            });
-          }}
+          onChange={OnSearchChange}
         />
 
         {filteredMonsters.map((monster) => {
